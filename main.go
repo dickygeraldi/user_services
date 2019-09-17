ea8c52160c19e2f15fdd1efd1d5b2c4c0d46e1c7
dickygeraldi@gmail.com <dickygeraldi@gmail.com>
2019-09-17 10:41:31 +0700

    Drone New Image 10

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8000" //localhost
+		port = "8080" //localhost
 	}
 
 	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
